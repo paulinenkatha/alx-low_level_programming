fd5db7d4c4c1ab0487eceb1b2386acbeeaaca501
paulinenkatha <paulinemyash@gmail.com>
2023-03-21 10:42:58 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -9,11 +9,11 @@ int main(void)
 {
     int r;
 
-    r = _islower('H');
+    r = _is_lower('H');
     _putchar(r + '0');
-    r = _islower('o');
+    r = _is_lower('o');
     _putchar(r + '0');
-    r = _islower(108);
+    r = _is_lower(108);
     _putchar(r + '0');
     _putchar('\n');
     return (0);
