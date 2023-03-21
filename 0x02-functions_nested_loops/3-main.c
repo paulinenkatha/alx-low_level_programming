ce47e554417c4d7c476cd92c94fdcc22265855db
paulinenkatha <paulinemyash@gmail.com>
2023-03-21 11:03:13 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -5,15 +5,16 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     int r;
 
-    r = _is_lower('H');
+    r = _islower('H');
     _putchar(r + '0');
-    r = _is_lower('o');
+    r = _islower('o');
     _putchar(r + '0');
-    r = _is_lower(108);
+    r = _islower(108);
     _putchar(r + '0');
     _putchar('\n');
     return (0);
