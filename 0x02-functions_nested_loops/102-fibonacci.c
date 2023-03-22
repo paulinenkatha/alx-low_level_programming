0b1adc16b117e7b7e3c62d39d931a143eea6d133
paulinenkatha <paulinemyash@gmail.com>
2023-03-21 18:41:50 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -17,12 +17,12 @@ int main(void)
 		sum = fib1 + fib2;
 		printf("%lu", sum);
 
-		fib1 = fib2
-			fib2 = sum
+		fib1 = fib2;
+			fib2 = sum;
 
 			if (count == 49)
 				printf("\n");
-			else
+			if else
 				printf(", ");
 	}
 	
