3b50b3c68deab41597a6fceb81f63279ccb77393
paulinenkatha <paulinemyash@gmail.com>
2023-03-21 19:39:41 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,7 +13,7 @@ int main(void)
 	unsigned long int i;
 		unsigned long int bef = 1;
 		unsigned long int aft = 2;
-		unsigned long int 1 = 1000000000;
+		unsigned long int l = 1000000000;
 		unsigned long int bef1;
 		unsigned long int bef2;
 		unsigned long int aft1;
@@ -28,10 +28,10 @@ int main(void)
 			bef = aft - bef;
 		}
 
-		bef1 = (bef / 1);
-		bef2 = (bef % 1);
-		aft1 = (aft / 1);
-		aft2 = (aft % 1);
+		bef1 = (bef / l);
+		bef2 = (bef % l);
+		aft1 = (aft / l);
+		aft2 = (aft % l);
 
 		for (i = 92; i < 99; ++i)
 		{
